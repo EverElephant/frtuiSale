f960366e1091fe12b4085b8ed19aa3476fbe595e
727845268@qq.com <727845268@qq.com>
2017-05-17 11:02:14 +0800

    555

--- src/main/java/com/elp/fruitSale/controller/buy/BuyController.java
+++ src/main/java/com/elp/fruitSale/controller/buy/BuyController.java
@@ -1,6 +1,7 @@
 package com.elp.fruitSale.controller.buy;
 
 
+import java.text.NumberFormat;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -39,10 +40,11 @@ public class BuyController {
 
 	@RequestMapping(value="trolley",method = RequestMethod.POST)
 	public String trolley(HttpServletRequest request,HttpServletResponse response,Model model){
+		NumberFormat nf = NumberFormat.getNumberInstance();
+		nf.setMaximumFractionDigits(1);
 		String cookiesStr = request.getHeader("Cookie");
 		if(null==cookiesStr||"".equals(cookiesStr)){
-			
-			return "buy/toBuy-trolley";
+			return "buy/toBuy-emptyTrolley";
 		}
 		String[] cookies = cookiesStr.split(";");
 		float total=0;
@@ -52,7 +54,6 @@ public class BuyController {
 		List<HashMap<String,Object>> prices = dao.queryAllPrice();
 		//购物车缓存String
 		String trolleysString="";
-
 		//如果有缓存
 		if(null!=cookies){
 			for(String c:cookies){
@@ -65,6 +66,9 @@ public class BuyController {
 		if(!trolleysString.equals("")){
 			@SuppressWarnings("unchecked")
 			List<JSONObject> list =(List<JSONObject>) JSON.parseObject(trolleysString, ArrayList.class);
+			if(list.size()==0){
+				return "buy/toBuy-emptyTrolley";
+			}
 			//看看购物车里有没有该商品，有则加数，无则添加
 			for(JSONObject p:list){
 				Product pro = new Product();			
@@ -76,17 +80,18 @@ public class BuyController {
 					if(type.equals(m.get("type"))){
 						pro.setName((String)m.get("name"));
 						pro.setPrice((float)m.get("price"));
-						
+						break;
 					}
 						
 				}
 				float smallTotal =pro.getNum()*pro.getPrice();
-				pro.setTotalPrice(smallTotal);
+				pro.setTotalPrice((float)(Math.round(smallTotal*10))/10);
 				total = total+smallTotal;
 				productList.add(pro);
 			}
 		}
-		model.addAttribute("total",total);
+		
+		model.addAttribute("total",nf.format(total));
 		model.addAttribute("productList", productList);
 		return "buy/toBuy-trolley";
 	}
--- src/main/resources/static/fruitSale/buy/js/trolley.js
+++ src/main/resources/static/fruitSale/buy/js/trolley.js
@@ -1,23 +1,122 @@
 function changeTrolley(dom){
 	var num =jQuery(dom).val()
-//	var type=jQuery(dom).parent().attr("value");
-//	jQuery.post("to_buy/change_trolley",{"type":type,"num":num},function(){
-//		jQuery("#listDiv").html(data);
-//	});
-	alert(num);
+	var type=jQuery(dom).parent().parent().children(":first").text();
+
+	add_to(type,Number(num));
+	calculate_total();
 }
 
 
 function add(dom) {
 	var a = jQuery(dom).prev().val();
+	var t = jQuery(dom).parent().parent().children(":first").text();
+	var p = jQuery(dom).parent().parent().children(":first").next().next().text();
     a++;
+    add_to(t,a);
     jQuery(dom).prev().val(a);
+    var oldsum = jQuery("#susum").text();
+    var newsum = parseFloat(oldsum)+parseFloat(p);
+    jQuery("#susum").text(newsum.toFixed(1));
+    
 }
 function sub(dom) {
 	var a = jQuery(dom).next().val();
+	var t = jQuery(dom).parent().parent().children(":first").text();
+	var p = jQuery(dom).parent().parent().children(":first").next().next().text();
 	if(a>1){
 		a--;
-	    jQuery(dom).next().val(a);
+		add_to(t,a);
+		jQuery(dom).next().val(a);
+		var oldsum = jQuery("#susum").text();
+	    var newsum = parseFloat(oldsum)-parseFloat(p);
+	    jQuery("#susum").text(newsum.toFixed(1));
 	}
-    
+	
+}
+function add_to(type,num){
+	// 用于保存购物车商品信息
+	var shopping_trolley = [];
+
+	// 先获取当前cookie
+	var cookies = document.cookie.split('; ');
+	for(var i=0;i<cookies.length;i++){
+		var arr = cookies[i].split('=');
+		if(arr[0] === 'shopping_trolley'){
+			shopping_trolley = JSON.parse(arr[1]);
+		}
+	}
+
+
+
+	for(var i=0;i<shopping_trolley.length;i++){
+		if(shopping_trolley[i].type == type){
+			shopping_trolley[i].num =num;
+			break;
+		}
+	}
+
+	// 存入cookie
+	// 把对象/数组转换诚json字符串：JSON.stringify()
+	var Days = 30;
+	var exp = new Date(); 
+	exp.setTime(exp.getTime() + Days*24*60*60*1000);
+	document.cookie = 'shopping_trolley=' + JSON.stringify(shopping_trolley) + ";expires=" + exp.toGMTString();
+
+}
+function calculate_total(){
+	var total=0.0;
+	jQuery("#listDiv tr").each(function(index) {
+		if(index!=0){
+			var p =jQuery(this).find(".price").text();
+			var n =jQuery(this).find(".num").children("input").val();
+			total = parseFloat(n)*parseFloat(p)+total;
+		}
+	})
+	jQuery("#susum").text(total.toFixed(1));
+}
+function remove_from(dom){
+	var tr =jQuery(dom).parent().parent();
+	var type = jQuery(tr).children(":first").text();
+	// 用于保存购物车商品信息
+	var shopping_trolley = [];
+
+	// 先获取当前cookie
+	var cookies = document.cookie.split('; ');
+	for(var i=0;i<cookies.length;i++){
+		var arr = cookies[i].split('=');
+		if(arr[0] === 'shopping_trolley'){
+			shopping_trolley = JSON.parse(arr[1]);
+		}
+	}
+
+
+
+	for(var i=0;i<shopping_trolley.length;i++){
+		if(shopping_trolley[i].type == type){
+			shopping_trolley.splice(i,1);
+			break;
+		}
+	}
+
+	// 存入cookie
+	// 把对象/数组转换诚json字符串：JSON.stringify()
+	var Days = 30;
+	var exp = new Date(); 
+	exp.setTime(exp.getTime() + Days*24*60*60*1000);
+	document.cookie = 'shopping_trolley=' + JSON.stringify(shopping_trolley) + ";expires=" + exp.toGMTString();
+	
+	
+	if(shopping_trolley.length!=0){
+		jQuery(tr).remove();
+		calculate_total();
+	}else{
+		jQuery(".row .table-bordered").parent().next().remove();
+		jQuery(".row .table-bordered").parent().html("<span>购物车为空</span>");	
+	}
+}
+
+function remove_all(){
+	document.cookie = 'shopping_trolley=[]';
+	jQuery(".row .table-bordered").parent().next().remove();
+	jQuery(".row .table-bordered").parent().html("<span>购物车为空</span>");	
 }
