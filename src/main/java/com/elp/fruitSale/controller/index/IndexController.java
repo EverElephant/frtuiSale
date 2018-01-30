a47d857851670967ccfa378adb056bba850a3b91
renhuiyu <renhuiyu@lohogames.com>
2018-01-30 16:15:56 +0800

    加入了server.context-path=/fruit 修改资源为相对目录

--- src/main/java/com/elp/fruitSale/controller/index/IndexController.java
+++ src/main/java/com/elp/fruitSale/controller/index/IndexController.java
@@ -3,15 +3,31 @@ package com.elp.fruitSale.controller.index;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 public class IndexController {
 	@RequestMapping("/")
-	public String home(){
+	public String home(HttpServletRequest request){
+		System.out.println(getIp(request));
 		return "index";
 	}
 	@RequestMapping("/index")
 	public String index(){
 		return "index";
+	}
+
+	public String getIp(HttpServletRequest request) {
+		String ip = request.getHeader("x-forwarded-for");
+		if (ip == null || ip.length() == 0 || ip.equalsIgnoreCase("unknown"))
+			ip = request.getHeader("Proxy-Client-IP");
+		if (ip == null || ip.length() == 0 || ip.equalsIgnoreCase("unknown"))
+			ip = request.getHeader("WL-Proxy-Client-IP");
+		if (ip == null || ip.length() == 0 || ip.equalsIgnoreCase("unknown"))
+			ip = request.getRemoteAddr();
+		if (ip == null || ip.length() == 0 || ip.equalsIgnoreCase("unknown"))
+			ip = "unknown";
+		return ip;
 	}
 
 }
